eb03684eed67a5959100c8ff077c4df9e1b35f7f
GugaNGomes <gugangomes@example.com>
2025-11-28 01:52:27 -0300

    e

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -21,9 +21,7 @@ function ForgotPassword({ isVisible, onClose, onBackToLogin }) {
         className="absolute top-4 left-4 flex items-center z-10 hover:opacity-80 transition-opacity cursor-pointer"
       >
         <img
-          src={
-            "/imgs/logoHeader.png"
-          }
+          src={"./imgs/logoHeader.png"}
           alt="MEU BOLSO"
           className="h-7 sm:h-10 w-auto"
         />
@@ -169,9 +167,7 @@ function ForgotPassword({ isVisible, onClose, onBackToLogin }) {
         {/* Phone with WhatsApp */}
         <div className="relative z-10 transform rotate-12 animate-float">
           <img
-            src={
-              "/imgs/celular.png"
-            }
+            src={"/imgs/celular.png"}
             alt="WhatsApp Meu Bolso"
             className="w-96 h-auto drop-shadow-2xl"
           />
